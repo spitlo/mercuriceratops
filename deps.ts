e1f42f921e0fda589666531555308fefc7c2404d
Jakob Bergman <jakob@aino.se>
2020-09-06 10:10:41 +0200

    Update std import syntax

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 import Kia from "https://deno.land/x/kia@0.3.0/mod.ts";
 
-export * as log from "https://deno.land/std@v0.54.0/log/mod.ts";
-export { BufReader } from "https://deno.land/std@v0.54.0/io/mod.ts";
-export { TextProtoReader } from "https://deno.land/std@v0.54.0/textproto/mod.ts";
+export * as log from "https://deno.land/std@0.54.0/log/mod.ts";
+export { BufReader } from "https://deno.land/std@0.54.0/io/mod.ts";
+export { TextProtoReader } from "https://deno.land/std@0.54.0/textproto/mod.ts";
 export {
   blue,
   bold,
@@ -15,7 +15,7 @@ export {
   underline,
   white,
   yellow,
-} from "https://deno.land/std@v0.54.0/fmt/colors.ts";
-export { parse } from "https://deno.land/std@v0.54.0/flags/mod.ts";
+} from "https://deno.land/std@0.54.0/fmt/colors.ts";
+export { parse } from "https://deno.land/std@0.54.0/flags/mod.ts";
 
 export { Kia };
